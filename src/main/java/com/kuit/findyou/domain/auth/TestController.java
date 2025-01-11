649a12053641701b5b28ae8b34156ec90a77bcd1
JangIkhwan <bluensky0213@gmail.com>
2025-01-11 17:17:01 +0900

    [feat] #9 response, controllerAdvice 구현

--- src/main/java/com/kuit/findyou/domain/auth/TestController.java
+++ src/main/java/com/kuit/findyou/domain/auth/TestController.java
@@ -1,5 +1,7 @@
 package com.kuit.findyou.domain.auth;
 
+import com.kuit.findyou.global.common.exception.BadRequestException;
+import com.kuit.findyou.global.common.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Operation(summary = "테스트", description = "테스트")
     @GetMapping("test")
-    public String hello(){
-        return "hello";
+    public BaseResponse<String> hello(){
+        return new BaseResponse<>("hello");
+    }
+
+    @GetMapping("test/error")
+    public BaseResponse<String> testControllerAdvice(){
+//        if(true) throw new BadRequestException(BAD_REQUEST);
+        if(true) throw new IllegalArgumentException();
+        return new BaseResponse<>("hello");
     }
 }
