27df001176defb9e716324db95cc0fc36bd7db25
ksg <ksg001227@naver.com>
2025-01-16 17:10:50 +0900

    [feat] #13 신고 동물 조회 필터링 메서드 구현

--- src/main/java/com/kuit/findyou/domain/report/repository/ReportRepository.java
+++ src/main/java/com/kuit/findyou/domain/report/repository/ReportRepository.java
@@ -5,10 +5,32 @@ import com.kuit.findyou.domain.report.model.Report;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     Slice<Report> findByIdLessThanOrderByIdDesc(Long id, Pageable pageable);
+
+
+    @Query("SELECT r FROM Report r " +
+    "WHERE r.id < :id " +
+    "AND (:startDate IS NULL OR r.eventDate >= :startDate) " +
+    "AND (:endDate IS NULL OR r.eventDate <= :endDate) " +
+    "AND (:species IS NULL OR r.reportAnimal.breed.species = :species) " +
+    "AND (:breeds IS NULL OR r.reportAnimal.breed.name IN :breeds) " +
+    "AND (:location IS NULL OR r.foundLocation LIKE CONCAT('%', :location, '%'))" +
+    "ORDER BY r.id DESC")
+    Slice<Report> findReportsWithFilters(@Param("id") Long id,
+                                         @Param("startDate") LocalDate startDate,
+                                         @Param("endDate") LocalDate endDate,
+                                         @Param("species") String species,
+                                         @Param("breeds") List<String> breeds,
+                                         @Param("location") String location,
+                                         Pageable pageable);
 }
--- src/main/java/com/kuit/findyou/domain/report/service/ReportAnimalRetrieveService.java
+++ src/main/java/com/kuit/findyou/domain/report/service/ReportAnimalRetrieveService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -20,21 +21,49 @@ public class ReportAnimalRetrieveService {
     private final ReportRepository reportRepository;
     private final UserRepository userRepository;
 
-    public ReportCardDTO retrieveReportCards(Long userId, Long lastReportId) {
+//    public ReportCardDTO retrieveReportCards(Long userId, Long lastReportId) {
+//        User loginedUser = userRepository.findById(userId)
+//                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+//
+//        Slice<Report> reportSlice = reportRepository.findByIdLessThanOrderByIdDesc(lastReportId, PageRequest.of(0, 20));
+//
+//        List<Card> cards = reportSlice.map(report ->
+//                Card.newInstanceFromReportWithUser(report, loginedUser))
+//                .getContent();
+//
+//        Boolean isLast = reportSlice.isLast();
+//
+//        Long newLastReportId = -1L;
+//        if(!cards.isEmpty()) {
+//            newLastReportId = cards.get(cards.size() - 1).getId();
+//        }
+//
+//        return ReportCardDTO.newInstance(cards, newLastReportId, isLast);
+//    }
+
+    public ReportCardDTO retrieveReportCardsWithFilters(
+            Long userId,
+            Long lastReportId,
+            LocalDate startDate,
+            LocalDate endDate,
+            String species,
+            List<String> breeds,
+            String location) {
+
         User loginedUser = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 
-        Slice<Report> reportSlice = reportRepository.findByIdLessThanOrderByIdDesc(lastReportId, PageRequest.of(0, 20));
+        Slice<Report> reportSlice = reportRepository.findReportsWithFilters(lastReportId, startDate, endDate, species, breeds, location, PageRequest.of(0, 20));
 
         List<Card> cards = reportSlice.map(report ->
-                Card.newInstanceFromReportWithUser(report, loginedUser))
+                        Card.newInstanceFromReportWithUser(report, loginedUser))
                 .getContent();
 
         Boolean isLast = reportSlice.isLast();
 
         Long newLastReportId = -1L;
         if(!cards.isEmpty()) {
-            newLastReportId = cards.get(cards.size() - 1).getId();
+            newLastReportId = cards.get(cards.size() - 1).getCardId();
         }
 
         return ReportCardDTO.newInstance(cards, newLastReportId, isLast);
