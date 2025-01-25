4dffb0f8d4c8e03e35f477116122c0cb0031ad90
ksg <ksg001227@naver.com>
2025-01-25 20:26:12 +0900

    [Feat] #34 최근 본 글 조회를 위한 데이터 조회 메서드 구현

--- src/main/java/com/kuit/findyou/domain/report/repository/ViewedProtectingReportRepository.java
+++ src/main/java/com/kuit/findyou/domain/report/repository/ViewedProtectingReportRepository.java
@@ -3,6 +3,8 @@ package com.kuit.findyou.domain.report.repository;
 import com.kuit.findyou.domain.auth.model.User;
 import com.kuit.findyou.domain.report.model.ProtectingReport;
 import com.kuit.findyou.domain.report.model.ViewedProtectingReport;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,7 @@ public interface ViewedProtectingReportRepository extends JpaRepository<ViewedPr
     // 특정 유저와 보호글을 기준으로 ViewedProtectingReport 조회
     Optional<ViewedProtectingReport> findByUserAndProtectingReport(User user, ProtectingReport protectingReport);
 
+    Slice<ViewedProtectingReport> findByUserAndProtectingReportIdLessThanOrderByProtectingReportIdDesc(User user, Long ProtectingReportId, Pageable pageable);
 }
 
 
