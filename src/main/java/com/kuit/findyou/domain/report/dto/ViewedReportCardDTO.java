fe0d6f49f0168a82a6ff57759718bf9ee5796712
ksg <ksg001227@naver.com>
2025-01-27 02:05:26 +0900

    [Fix] #34 최근 본 동물 조회용 DTO 별도 생성

--- src/main/java/com/kuit/findyou/domain/report/dto/ViewedReportCardDTO.java
+++ src/main/java/com/kuit/findyou/domain/report/dto/ViewedReportCardDTO.java
@@ -1,2 +1,112 @@
-package com.kuit.findyou.domain.report.dto;public class ViewedReportCardDTO {
+package com.kuit.findyou.domain.report.dto;
+
+import com.kuit.findyou.domain.auth.model.User;
+import com.kuit.findyou.domain.report.model.ProtectingReport;
+import com.kuit.findyou.domain.report.model.Report;
+import com.kuit.findyou.domain.report.model.ViewedProtectingReport;
+import com.kuit.findyou.domain.report.model.ViewedReport;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@Builder
+public class ViewedReportCardDTO {
+
+    private List<Card> viewedAnimals;
+    private Long lastViewedProtectId;
+    private Long lastViewedReportId;
+    private Boolean isLast;
+
+    public static ViewedReportCardDTO newInstanceWithShuffle(List <Card> cards, Long newLastViewedProtectId, Long newLastViewedReportId, Boolean isLast){
+        Collections.shuffle(cards);
+
+        return ViewedReportCardDTO.builder()
+                .viewedAnimals(cards)
+                .lastViewedProtectId(newLastViewedProtectId)
+                .lastViewedReportId(newLastViewedReportId)
+                .isLast(isLast)
+                .build();
+
+    }
+
+    public static ViewedReportCardDTO mergeCards(List<ViewedProtectingReport> viewedProtectingReportList, List<ViewedReport> viewedReportList, User loginedUser) {
+        List<Card> viewedAnimals = new ArrayList<>();
+
+        int viewedProtectingReportIndex = 0;
+        int viewedReportIndex = 0;
+
+        Boolean isLast = isLast(viewedProtectingReportList, viewedReportList);
+
+        Long newLastViewedProtectId = -1L;
+        Long newLastViewedReportId = -1L;
+
+        while (viewedProtectingReportIndex <= viewedProtectingReportList.size() - 1 && viewedReportIndex <= viewedReportList.size() - 1) {
+            ViewedProtectingReport viewedProtectingReport = viewedProtectingReportList.get(viewedProtectingReportIndex);
+            ViewedReport viewedReport = viewedReportList.get(viewedReportIndex);
+
+            ProtectingReport protectingReport = viewedProtectingReport.getProtectingReport();
+            Report report = viewedReport.getReport();
+
+            newLastViewedProtectId = viewedProtectingReport.getId();
+            newLastViewedReportId = viewedReport.getId();
+
+            Card protectingCard = Card.newInstanceFromProtectingReportWithUser(protectingReport, loginedUser);
+
+            Card reportCard = Card.newInstanceFromReportWithUser(report, loginedUser);
+
+            viewedAnimals.add(protectingCard);
+            viewedAnimals.add(reportCard);
+
+            viewedProtectingReportIndex++;
+            viewedReportIndex++;
+
+            if (viewedAnimals.size() == 20) {
+                return newInstanceWithShuffle(viewedAnimals, newLastViewedProtectId, newLastViewedReportId, isLast);
+            }
+        }
+
+        while (viewedProtectingReportIndex <= viewedProtectingReportList.size() - 1) {
+            ViewedProtectingReport viewedProtectingReport = viewedProtectingReportList.get(viewedProtectingReportIndex);
+            ProtectingReport protectingReport = viewedProtectingReport.getProtectingReport();
+
+            newLastViewedProtectId = viewedProtectingReport.getId();
+
+            Card protectingCard = Card.newInstanceFromProtectingReportWithUser(protectingReport, loginedUser);
+
+            viewedAnimals.add(protectingCard);
+
+            viewedProtectingReportIndex++;
+
+            if (viewedAnimals.size() == 20) {
+                return newInstanceWithShuffle(viewedAnimals, newLastViewedProtectId, newLastViewedReportId, isLast);
+            }
+        }
+
+        while (viewedReportIndex <= viewedReportList.size() - 1) {
+            ViewedReport viewedReport = viewedReportList.get(viewedReportIndex);
+            Report report = viewedReport.getReport();
+
+            newLastViewedReportId = viewedReport.getId();
+
+            Card reportCard = Card.newInstanceFromReportWithUser(report, loginedUser);
+
+            viewedAnimals.add(reportCard);
+
+            viewedReportIndex++;
+
+            if (viewedAnimals.size() == 20) {
+                return newInstanceWithShuffle(viewedAnimals, newLastViewedProtectId, newLastViewedReportId, isLast);
+            }
+        }
+
+        return newInstanceWithShuffle(viewedAnimals, newLastViewedProtectId, newLastViewedReportId, isLast);
+    }
+
+    private static Boolean isLast(List<ViewedProtectingReport> viewedProtectingReportList, List<ViewedReport> viewedReportList) {
+        return viewedProtectingReportList.size() + viewedReportList.size() <= 20;
+    }
 }
