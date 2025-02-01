3cabaf11cdf599a462ffda9d7217d3be420bb1a2
ksg <ksg001227@naver.com>
2025-02-02 03:37:56 +0900

    [Rename] #50 메서드 명 변경

--- src/main/java/com/kuit/findyou/domain/api/controller/ProtectAnimalApiController.java
+++ src/main/java/com/kuit/findyou/domain/api/controller/ProtectAnimalApiController.java
@@ -1,6 +1,6 @@
 package com.kuit.findyou.domain.api.controller;
 
-import com.kuit.findyou.domain.api.service.ProtectAnimalApiService;
+import com.kuit.findyou.domain.api.service.ProtectAnimalApiUtil;
 import com.kuit.findyou.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProtectAnimalApiController {
 
-    private final ProtectAnimalApiService protectAnimalApiService;
+    private final ProtectAnimalApiUtil protectAnimalApiService;
 
     @GetMapping("/api/v1/initDatabase")
-    public BaseResponse<Void> getAbandonmentData() {
+    public BaseResponse<Void> initDatabase() {
 
         protectAnimalApiService.updateAllProtectingReports();
 
