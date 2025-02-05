361dedbb0246eb1dd97e8fc868d57e1a57e1f6e2
ksg <ksg001227@naver.com>
2025-02-05 20:23:41 +0900

    [Fix] #58 전체 조회 명세 수정

--- src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
+++ src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
@@ -98,7 +98,7 @@ public class ReportController {
         return new BaseResponse<>(reportCardDTO);
     }
 
-    @Operation(summary = "신고 동물 조회", description = "신고 동물들의 정보를 조회합니다.")
+    @Operation(summary = "전체 조회", description = "모든 동물들의 정보를 조회합니다.")
     @GetMapping
     public BaseResponse<TotalCardDTO> retrieveAll(
             @Parameter(description = "이전 요청을 통해 받아온 데이터들 중 마지막 보호글의 ID 입니다. 이 값을 다음 요청에 포함시키면 그 다음 보호글들을 조회하여 응답합니다.")
