b4a2fd71f80ac16e932511e9978c43908224544a
JangIkhwan <bluensky0213@gmail.com>
2025-02-09 00:25:20 +0900

    [Docs] #64 홈화면 조회의 응답 DTO에 명세 추가

--- src/main/java/com/kuit/findyou/domain/home/dto/HomeProtectAnimalCard.java
+++ src/main/java/com/kuit/findyou/domain/home/dto/HomeProtectAnimalCard.java
@@ -1,18 +1,31 @@
 package com.kuit.findyou.domain.home.dto;
 
 import com.kuit.findyou.domain.report.model.ProtectingReport;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDate;
 
+@Schema(description = "홈화면 보호중동물 조회의 응답 DTO")
 @Getter
 @Builder
 public class HomeProtectAnimalCard {
+    @Schema(description = "보호중동물 id")
     private Long protectId;
+
+    @Schema(description = "썸네일 이미지 url")
     private String thumbnailImageUrl;
+
+    @Schema(description = "제목")
     private String title;
+
+    @Schema(description = "태그. 값은 보호중임")
     private String tag;
+
+    @Schema(description = "공고시작일")
     private LocalDate noticeStartDate;
+
+    @Schema(description = "보호장소")
     private String careAddress;
 
     public static HomeProtectAnimalCard entityToDto(ProtectingReport entity){
