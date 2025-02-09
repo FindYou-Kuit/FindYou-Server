57829bf9086e4c5b925ef76f67894dac2ddede0a
ksg <ksg001227@naver.com>
2025-02-10 02:32:39 +0900

    [Fix] #78 불필요한 캐시매니저 삭제

--- src/main/java/com/kuit/findyou/global/config/RedisCacheConfig.java
+++ src/main/java/com/kuit/findyou/global/config/RedisCacheConfig.java
@@ -18,37 +18,8 @@ import java.time.Duration;
 @EnableCaching // Spring Boot 의 캐싱 설정 활성화
 public class RedisCacheConfig {
 
-    // 만료시간 구분을 위해 CacheManager를 구분
-
-    // 구조 동물, 신고 동물 상세 정보 저장을 위한 CacheManager
-    @Primary
-    @Bean
-    public CacheManager reportInfoCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
-                .defaultCacheConfig()
-                // Redis 에 Key 를 저장할 때 String 으로 직렬화(변환) 해서 저장
-                .serializeKeysWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new StringRedisSerializer()
-                        ))
-                // Redis 에 Value 를 저장할 때 JSON 으로 직렬화(변환) 해서 저장
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new Jackson2JsonRedisSerializer<Object>(Object.class)
-                        )
-                )
-                .entryTtl(Duration.ofMinutes(30L)); // 만료 시간 = 30분
-
-
-        return RedisCacheManager
-                .RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
-
-
     // 품종 정보들을 캐싱
+    @Primary
     @Bean
     public CacheManager breedCacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
@@ -71,7 +42,4 @@ public class RedisCacheConfig {
                 .cacheDefaults(redisCacheConfiguration)
                 .build();
     }
-
-
-    // 신고글, 보호글에 대한 카드 정보들은 캐싱 적용 여부 고려중
 }
