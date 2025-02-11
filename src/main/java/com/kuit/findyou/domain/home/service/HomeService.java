f00f4e3e5bc8dca2a1a9359436efe9729f54dc2d
JangIkhwan <bluensky0213@gmail.com>
2025-02-11 17:03:46 +0900

    [Fix] #75 변수명 수정

--- src/main/java/com/kuit/findyou/domain/home/service/HomeService.java
+++ src/main/java/com/kuit/findyou/domain/home/service/HomeService.java
@@ -22,7 +22,7 @@ public class HomeService {
     private final ProtectingReportRepository protectingReportRepository;
     private final ReportRepository reportRepository;
     public GetHomeDataResponse getHomeData() {
-        Long todayProtectCount = protectingReportRepository.countByHappenDateEquals(LocalDate.now().minusDays(1)); // 오늘 추가된 보호글 개수 계산
+        Long yesterdayProtectCount = protectingReportRepository.countByHappenDateEquals(LocalDate.now().minusDays(1)); // 어제 추가된 보호글 개수 계산
         Long todayReportCount = reportRepository.countByCreatedAtBetween(LocalDate.now().atStartOfDay(), LocalDate.now().atTime(LocalTime.MAX));  // 오늘 추가된 신고글 개수 계산
         List<ProtectingReport> recent10Protects = protectingReportRepository.findTop10ByOrderByCreatedAtDesc();
         List<Report> recent10Reports = reportRepository.findTop10ByOrderByCreatedAtDesc();
@@ -31,7 +31,7 @@ public class HomeService {
         List<HomeProtectAnimalCard> protectAnimalCards = recent10Protects.stream().map(HomeProtectAnimalCard::entityToDto).collect(toList());
         List<HomeReportAnimalCard> reportAnimalCards = recent10Reports.stream().map(HomeReportAnimalCard::entityToDto).collect(toList());
         return GetHomeDataResponse.builder()
-                .yesterdayRescuedAnimalCount(todayProtectCount)
+                .yesterdayRescuedAnimalCount(yesterdayProtectCount)
                 .todayReportAnimalCount(todayReportCount)
                 .protectAnimalCards(protectAnimalCards)
                 .reportAnimalCards(reportAnimalCards)
