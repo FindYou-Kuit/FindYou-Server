639d23fd76aae9f141eea88f411614e5a0680637
JangIkhwan <bluensky0213@gmail.com>
2025-02-14 12:02:29 +0900

    [Feat] #87 시큐리티 설정

---
+++ src/main/java/com/kuit/findyou/global/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.kuit.findyou.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth)->auth.disable());
+
+        http
+                .httpBasic((auth)->auth.disable());
+
+        http
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/api/v1/auth/signup").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
