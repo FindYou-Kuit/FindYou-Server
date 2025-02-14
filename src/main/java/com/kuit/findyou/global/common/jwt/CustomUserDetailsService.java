ae4fdd50354ab68a138136d3d9defc9a5ccffe24
JangIkhwan <bluensky0213@gmail.com>
2025-02-14 19:28:40 +0900

    [Feat] #87 커스텀 UserDetail 관련 클래스들 작성

---
+++ src/main/java/com/kuit/findyou/global/common/jwt/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.kuit.findyou.global.common.jwt;
+
+import com.kuit.findyou.domain.user.model.User;
+import com.kuit.findyou.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        User user = userRepository.findByEmail(username);
+        System.out.println("user = " + user);
+
+        if (user != null) {
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(user);
+        }
+
+        return null;
+    }
+}
