990742ac8a048ef444d4bdeef0ff91fb8367786e
JangIkhwan <bluensky0213@gmail.com>
2025-02-18 21:48:24 +0900

    [Fix] #100 보호중동물id를 이용하여서 보호중동물에 대한 관심동물 해제를 수행하도록 수정

--- src/main/java/com/kuit/findyou/domain/user/service/UserService.java
+++ src/main/java/com/kuit/findyou/domain/user/service/UserService.java
@@ -103,8 +103,8 @@ public class UserService {
         return interestProtectingReportRepository.existsByUserIdAndProtectingReportId(userId, protectingReportId);
     }
 
-    public void removeInterestProtectingAnimal(Long userId, Long interestId) {
-        InterestProtectingReport interest = interestProtectingReportRepository.findById(interestId).orElseThrow(() -> new InterestAnimalNotFoundException(INTEREST_ANIMAL_NOT_FOUND));
+    public void removeInterestProtectingAnimal(Long userId, Long protectingReportId) {
+        InterestProtectingReport interest = interestProtectingReportRepository.findByUserIdAndProtectingReportId(userId, protectingReportId).orElseThrow(() -> new InterestAnimalNotFoundException(INTEREST_ANIMAL_NOT_FOUND));
         if (interest.getUser().getId() != userId){
             throw new UnauthorizedUserException(UNAUTHORIZED_USER);
         }
